c2eb1957e07ba782399841ca8ecd090831dd2b5c
Maksym Zyza <mmziza88@gmail.com>
2021-03-23 13:57:35 +0200

    добавлено createStore

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './redux/store';
 import 'modern-normalize/modern-normalize.css';
 import './base.css';
+
+console.log(store); //?
 
 ReactDOM.render(
   <React.StrictMode>
